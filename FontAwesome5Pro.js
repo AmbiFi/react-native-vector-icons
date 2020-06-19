74c41a42f63f500256c89f6f30009ec113342fef
Harald Scheckenbacher <harald.scheckenbacher@ambifi.com>
2020-06-19 13:54:40 -0400

    Initial changes

--- FontAwesome5Pro.js
+++ FontAwesome5Pro.js
@@ -9,15 +9,15 @@ import glyphMap from './glyphmaps/FontAwesome5Pro.json';
 import metadata from './glyphmaps/FontAwesome5Pro_meta.json';
 
 export { FA5Style } from './lib/create-icon-set-from-fontawesome5';
-
+console.log("FONT AWESOME 5 PRO", glyphMap["hands-wash"]);
 const iconSet = createFA5iconSet(glyphMap, metadata, true);
 
 export default iconSet;
 export const {
-  Button,
-  TabBarItem,
-  TabBarItemIOS,
-  ToolbarAndroid,
-  getImageSource,
-  getImageSourceSync,
+	Button,
+	TabBarItem,
+	TabBarItemIOS,
+	ToolbarAndroid,
+	getImageSource,
+	getImageSourceSync,
 } = iconSet;
